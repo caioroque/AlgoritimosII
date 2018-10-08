60f9331d6e5c2c12c20a5f6cb362f8bb54fa91d0
Thiago Costa <thiagorcosta011@gmail.com>
2018-10-08 09:39:33 -0300

    variaveis globais

---
+++ Aulas/Aula0810/Aula0810.cpp
@@ -0,0 +1,20 @@
+// Aula0810.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int a;
+float b;
+double c;
+bool d;
+
+
+int main()
+{
+	int e;
+    return 0;
+}
+
