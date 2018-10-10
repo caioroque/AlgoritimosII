66ca7965e905f0553ca357848f31d32152dae16d
Thiago Costa <thiagorcosta011@gmail.com>
2018-10-10 08:47:47 -0300

    scanf

--- Aulas/Aula0810/Aula0810.cpp
+++ Aulas/Aula0810/Aula0810.cpp
@@ -18,8 +18,25 @@ int main()
 
 	printf("BORAAAAAAA\n");
 	printf("valor total:%i\n", 20 + 10);
+
+	a = 20;
+	e = 30;
+
+	int f = a + e;
+
+	printf("o resultado é: %i\n", f);
+
+	scanf_s("%i", &f);
+	printf("vc digitou %i\n", f);
+
+	printf("digite um valor:");
+	scanf_s("%f", &b);
+	printf("o resultado é: %i\n", b);
 	
 	system("pause");
+
+
+
     return 0;
 }
 
