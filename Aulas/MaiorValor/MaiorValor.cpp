55a13b80de2d58ff315816201b3234c8f834375f
Thiago Costa <thiagorcosta011@gmail.com>
2018-10-22 10:46:02 -0300

    Media

--- Aulas/MaiorValor/MaiorValor.cpp
+++ Aulas/MaiorValor/MaiorValor.cpp
@@ -9,46 +9,50 @@ void HalloWord() {
 	printf("EAAAIIII\n");
 }
 
-int lerValor() {
-	int variavel = 0;
+float lerValor() {
+	float variavel = 0;
 	printf(" Numero:");
-	scanf_s("%i", &variavel);
+	scanf_s("%f", &variavel);
 	return variavel;
 }
+float MediaValor(float a, float b, float c, float d) {
 
-int Maior(int a, int b, int c, int d, int media) {
-	
-	if (a > media) {
-		return a;
-	}
-	else if (b > media) {
-		return b;
-	}
-	else if (c > media) {
-		return c;
-	}
-	else if( d > media){
-		return d;
-	}
-	else {
-		printf("nenhum valor e maior que a media!");
-	}
-}
-int Media(int a, int b, int c, int d) {
-	 
-	int media = a + b + c + d / 4;
+	float media = a + b + c + d / 4;
 	return media;
 
 }
 
+void MaiorDoQueMedia(float a, float b, float c, float d) {
+
+	float media = MediaValor(a, b, c, d);
+
+	printf("O valor da media eh: %f\nOs valores maiores das medias sao:", media);
+	if (a > media) {
+		printf("%f", a);
+	}
+	 if (b > media) {
+		printf("%f", b);
+	}
+	if (c > media) {
+		printf("%f", c);
+	}
+	 if (d > media) {
+		printf("%f", d);
+	}
+	
+}
+
+
 int main()
 {
-	int primeiroValor = lerValor();
-	int segundoValor = lerValor();
-	int terceiroValor = lerValor();
-	int quartoValor = lerValor();
-	int f = Maior(primeiroValor, segundoValor, terceiroValor, quartoValor);
-	printf("O Maior valor e: %i", f);
+
+	float primeiroValor = lerValor();
+	float segundoValor = lerValor();
+	float terceiroValor = lerValor();
+	float quartoValor = lerValor();
+	//float f = Media(primeiroValor, segundoValor, terceiroValor, quartoValor);
+	MaiorDoQueMedia(primeiroValor, segundoValor, terceiroValor, quartoValor);
+	
 	system("pause");
 	return 0;
 }
