dea4e6930b905bc4cbb3ed5bb271d2e085f7942d
Thiago Costa <thiagorcosta011@gmail.com>
2018-10-31 09:27:25 -0300

    ex

--- Aulas/EXALG2/EXALG2.cpp
+++ Aulas/EXALG2/EXALG2.cpp
@@ -48,5 +48,42 @@ int lerValor() {
 
 
 //EX3
+//int resto(int x, int y);
+//int main(void);
+//
+//int resto(int x, int y) {
+//	int q, r;
+//	q = x / y;
+//	r = x - q * y;
+//	return(r);
+//}
+//
+//int main(void) {
+//	int x, anos, dias, y, w, meses, diass;
+//	dias = lerValor();
+//	x = resto(dias, 365);
+//	if (x == 0)
+//	{
+//		anos = (dias / 365);
+//		printf("%d ano(s) e 0 mes(es) e 0 dia(s)", anos);
+//	}
+//	else
+//	{
+//		anos = (dias / 365);
+//		y = resto(dias, 365);
+//		w = resto(y, 30);
+//		if (w = 0)
+//		{
+//			meses = y / 30;
+//			printf("%d ano(s) e %d mes(es) e 0 dia(s)", anos, meses);
+//		}
+//		else
+//		{
+//			anos = dias / 365;
+//			meses = (y / 30);
+//			diass = resto(y, 30);
+//			printf("%d ano(s) e %d mes(es) e %d dia(s)", anos, meses, diass);
+//		}
+//	}
 //
 
