668821dbe663b6e54016df717db1c6171219fd94
Thiago Costa <thiagorcosta011@gmail.com>
2018-10-31 09:25:37 -0300

    Exercicios

---
+++ Aulas/EXALG2/EXALG2.cpp
@@ -0,0 +1,52 @@
+// EXALG2.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int lerValor() {
+	int variavel = 0;
+	printf(" Numero:");
+	scanf_s("%i", &variavel);
+	return variavel;
+}
+//EX1
+//int main()
+//{
+//	float raiz;
+//	int d;
+//	int x1 = lerValor();
+//	int x2 = lerValor();
+//	int y1 = lerValor();
+//	int y2 = lerValor();
+//	d = (x2*x2 - 2 * x1*x2 + x1*x1) + (y2*y2 - 2 * y2*y1 + y1*y1);
+//	raiz = sqrt(d);
+//	printf("RESPOSTA:%0.2f", raiz);
+//	system("pause");
+//    return 0;
+//}
+
+//EX2
+//int main()
+//{
+//	float Z;
+//	int  R, S, D;
+//	int A = lerValor();
+//	int B = lerValor();
+//	int C = lerValor();
+//	R = (A*A) + (B*B);
+//	S = (B*B )+ (C*C);
+//	D = R + S;
+//	Z = (D / 2);
+//	printf("RESPOSTA:%0.2f", Z);
+//	system("pause");
+//	return 0;
+//
+//}
+
+
+//EX3
+//
+
