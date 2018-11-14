02ff76ced092b099dd364cdd8d24e91bb13eda25
Thiago Costa <thiagorcosta011@gmail.com>
2018-11-14 09:34:38 -0200

    atualizado ponteiro

--- Aulas/EXPONTEIRO/EXPONTEIRO.cpp
+++ Aulas/EXPONTEIRO/EXPONTEIRO.cpp
@@ -2,10 +2,48 @@
 //
 
 #include "stdafx.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <locale.h>
 
-
-int main()
-{
-    return 0;
+void troca(float *a, float *b) {
+	float temp; 
+	temp = *a; 
+	*a = *b;
+	*b = temp;
 }
+
+
+void MaximoMinimo(int *v, int n, int *min, int *max) {
+	int i;
+	*min = v[0];
+	*max = v[0];
+	for (i = 1; i < n; i++) {
+		if (v[i] > *max) {
+			*max = v[i];
+		}
+		else if ((v[i] < *min)){
+			*min = v[i];
+		}
+	}
+}
+
+int main() {
+	int n, i, *vet, minimo, maximo;
+	printf("Quantos numeros voce deseja digitar? ");
+	scanf("%d", &n);
+	vet == malloc(n * sizeof(int));
+	for (i = 0; i < n; i++) {
+		printf("Digite o numero de indice %d: ", i); 
+		scanf("%d", &vet[i]);
+	}
+	MaximoMinimo(vet, n, &minimo, &maximo);
+	printf("Minimo: %d. Maximo: %d.\n", minimo, maximo); 
+	system("pause");
+	return 0;
+}
+
+	
+
 
